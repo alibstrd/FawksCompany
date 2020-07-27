a3dddff658e9b9f88e5fd0ae351747f2f8ba0816
Alidzulfikar <ayouthnotwasted@gmail.com>
2020-07-27 21:52:28 +0700

    Setting up the document changes

--- FawksCompany/Controller/HomeVC.swift
+++ FawksCompany/Controller/HomeVC.swift
@@ -46,12 +46,11 @@ class HomeVC: UIViewController {
                 }
             }
         }
+        setCategoriesListener()
         
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        //fetchDocument()
-        fetchCollection()
         
         if let user = Auth.auth().currentUser, !user.isAnonymous {
             loginOutBtn.title = "Logout"
@@ -61,41 +60,57 @@ class HomeVC: UIViewController {
         
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+         super.viewWillDisappear(animated)
+         listener.remove()
+     }
+ 
+    private func setCategoriesListener() {
+        listener = db.collection("categories").addSnapshotListener({ (snap, error) in
+            
+            if let error = error {
+                debugPrint(error.localizedDescription)
+                return
+            }
+            
+            snap?.documentChanges.forEach({ (change) in
+                
+                let data = change.document.data()
+                let category = Category.init(data: data)
+                
+                switch change.type {
+                case .added:
+                    self.onDocumentAdded(change: change, category: category)
+                case .modified:
+                    self.onDocumentModified(change: change, category: category)
+                case .removed:
+                    self.onDocumentRemoved(change: change, category: category)
+                }
+            })
+        })
+    }
+    
+    private func onDocumentAdded(change: DocumentChange, category: Category) {
+        let newIndex = Int(change.newIndex)
+        categories.insert(category, at: newIndex)
+        collectionView.insertItems(at: [IndexPath(item: newIndex, section: 0)])
+    }
+    
+    private func onDocumentModified(change: DocumentChange,category: Category) {
+        
+    }
+    
+    private func onDocumentRemoved(change: DocumentChange, category: Category) {
+        
+    }
+    
     fileprivate func presentLoginController() {
         let storyboard = UIStoryboard(name: Storyboard.LoginStoryboard, bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: StoryboardId.LoginVC)
         present(controller, animated: true, completion: nil)
     }
     
-    private func fetchDocument() {
-        let docRef = db.collection("categories").document("ZsLLvAEaB18ho7kJRz6g")
-        
-        // to listen to real time data changes
-        listener = docRef.addSnapshotListener { (snap, error) in
-            guard let data = snap?.data() else { return }
-            // to avoid create multiple documents
-            self.categories.removeAll()
-            let category = Category.init(data: data)
-            self.categories.append(category)
-            self.collectionView.reloadData()
-        }
-    }
-    
-    private func fetchCollection() {
-        let collectionRef = db.collection("categories")
-        
-        // to listen to real time data changes
-        listener = collectionRef.addSnapshotListener { (snap, error) in
-            guard let documents = snap?.documents else { return }
-            self.categories.removeAll()
-            for document in documents {
-                let data = document.data()
-                let category = Category.init(data: data)
-                self.categories.append(category)
-            }
-            self.collectionView.reloadData()
-        }
-    }
+
     
     @IBAction func loginOutBtnPressed(_ sender: UIBarButtonItem) {
         
@@ -139,11 +154,6 @@ class HomeVC: UIViewController {
         }
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        listener.remove()
-    }
-    
 }
 
 extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -172,3 +182,36 @@ extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
     }
 }
 
+
+
+// This method is basically just for prior knowledge
+
+//private func fetchDocument() {
+//    let docRef = db.collection("categories").document("ZsLLvAEaB18ho7kJRz6g")
+//
+//    // to listen to real time data changes
+//    listener = docRef.addSnapshotListener { (snap, error) in
+//        guard let data = snap?.data() else { return }
+//        // to avoid create multiple documents
+//        self.categories.removeAll()
+//        let category = Category.init(data: data)
+//        self.categories.append(category)
+//        self.collectionView.reloadData()
+//    }
+//}
+//
+//private func fetchCollection() {
+//    let collectionRef = db.collection("categories")
+//
+//    // to listen to real time data changes
+//    listener = collectionRef.addSnapshotListener { (snap, error) in
+//        guard let documents = snap?.documents else { return }
+//        self.categories.removeAll()
+//        for document in documents {
+//            let data = document.data()
+//            let category = Category.init(data: data)
+//            self.categories.append(category)
+//        }
+//        self.collectionView.reloadData()
+//    }
+//}
